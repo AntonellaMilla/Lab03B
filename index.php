734ab76ea9b6b47b3b62966161b1350184b86f9d
AriadnaNolasco <ariadna.nolasco@tecsup.edu.pe>
2024-09-19 20:06:11 -0500

    Ultimo cambio

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-session_start():
+session_start();
+
+if (!isset($_SESSION¨['tareas'])) {
+    $_SESSION['tareas']=[];
+
+}
+$tareas =$_SESSION['tareas'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,14 +16,25 @@ session_start():
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Mis Tareas</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Tareas</th>
-            </tr>
-        </thead>
-    </table>
+    <div class="container">
+        <h1>Mis Tareas</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Tareas</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                for($i=0;$i<count($tareas);$i++)
+                {
+                    echo '<tr><td>'.$tareas[$i].'<\td><\tr>';
+                }
+            ?>
+            </tbody>
+        </table>
+        <a href="agregar.html" class="btn btn-primary">Agregar Tareas</a>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
